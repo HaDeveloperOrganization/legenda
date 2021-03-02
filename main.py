3f5f22c7a26ea82d19acf042383bba3fc7157c8e
Pernesz Mátyás <perneszmatyi@gmail.com>
2021-03-02 10:24:52 +0000

    Made embed_display mention the user

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 import os
 import asyncio
 import re
+from discord.ext import commands
+from discord.utils import get
 
 
 client = discord.Client()
@@ -12,28 +14,31 @@ grv_commands = ['-p', '-play', '-skip', '-stop', '-resume', '-volume']
 @client.event
 async def on_ready():
   print(f'We have logged in as {client}')
-  print('Peri')
-
-@client.event
-async def on_message(message):
-  if message.author.id != 234395307759108106 and message.author != client.user:
-    print('ezpez')
-
+  user = await client.fetch_user(224506156272451586)
   
 
-  if message.author.id == 234395307759108106:
+@client.event
+async def on_message(message):  
+
+  if message.author.id == 234395307759108106: #groovy's id
+    
 
     for embed in message.embeds:
       embed_content = re.findall('(?<=\[)(.*?)(?=\])',embed.to_dict()['description'])[0]
       embed_author_id = re.findall('(?<=\[)(.*?)(?=\])',embed.to_dict()['description'])[-1][2:-1]
+      embed_author = await client.fetch_user(embed_author_id)
       embed_link = ''.join(re.findall('(?<=\()(http.*?)(?=\))', embed.to_dict()['description']))
       await message.delete()
+
+    print(embed_author_id)
     
-    embed=discord.Embed(title=embed_content, url= embed_link, description= '', color=0x02547e)
-    embed.set_author(name='A következő nóta:', url='')
-    embed.set_footer(text = f'{embed_author_id} kérésére')
-    print(message.author)
-    await message.channel.send(embed=embed)
+
+    
+    
+    embed_display=discord.Embed(title=embed_content, url= embed_link, description= '', color=0x02547e)
+    embed_display.set_author(name='A következő nóta:', url='')
+    embed_display.set_footer(text = f'{embed_author.name} kérésére')
+    await message.channel.send(embed=embed_display)
     
  
 
@@ -45,6 +50,7 @@ async def on_message(message):
 
 
   if message.channel.id != joska_id and re.findall('[^\s]*',message.content.lower())[0] in grv_commands or message.content.lower() in grv_commands:
+
     await asyncio.sleep(2)
     await message.delete()
     await message.channel.send(f'Joskaba irjad a zenet te {message.author.mention}!')
